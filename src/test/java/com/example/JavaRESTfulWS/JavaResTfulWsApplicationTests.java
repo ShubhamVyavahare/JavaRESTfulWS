2653fc58627123f4170784bdb88f9bf09765b8a3
ShubhamVyavahare <shubhamvyavahare203@gmail.com>
2020-04-30 21:54:48 +0530

    Basic Gradle Project For RESTful WS

---
+++ src/test/java/com/example/JavaRESTfulWS/JavaResTfulWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaRESTfulWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResTfulWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
